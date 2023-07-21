33dd0917a6a670a58580fd493070861b06a502d6
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-07-21 15:45:22 +0400

    new UI

--- T9_WPF/MainWindow.xaml.cs
+++ T9_WPF/MainWindow.xaml.cs
@@ -23,6 +23,44 @@ namespace T9_WPF
         public MainWindow()
         {
             InitializeComponent();
+            SynvoOfMascaras();
+            
+
+        }
+
+        public void SynvoOfMascaras()
+        {
+            toparrow.Content = $"^";
+            leftarrow.Content = $"<";
+            rightarrow.Content = $">";
+            bottomarrow.Content = $"\\/";
+            one.Content = $"1";
+            two.Content = $"   2\nABC";
+            three.Content = $"   3\nDEF";
+            four.Content = $"   4\nGHI";
+            five.Content = $"  5\nJKL";
+            six.Content = $"   6\nMNO";
+            seven.Content = $"   7\nPQRS";
+            eight.Content = $"   8\nTUV";
+            nine.Content = $"   9\nWXYZ";
+            zero.Content = $"0\n+";
+
+
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+
+                MessageBox.Show("salam");
+ 
+        }
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D1)
+            {
+                
+                MessageBox.Show("NumPad1 key pressed!");
+            }
         }
     }
 }
