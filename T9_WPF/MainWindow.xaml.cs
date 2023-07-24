9e56c4c086bbffd399997aab691dffa02c373b1d
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-07-24 21:36:57 +0400

    eror

--- T9_WPF/MainWindow.xaml.cs
+++ T9_WPF/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,12 +22,188 @@ namespace T9_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<char, string> t9Mappings = new Dictionary<char, string>
+        {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+
+        private StringBuilder inputBuffer = new();
+        private List<string> Vocabulary = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            StartFillVocabulary();
             SynvoOfMascaras();
-            
+        }
 
+        private void StartFillVocabulary()
+        {
+            string text = File.ReadAllText(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Vocabulary.txt"));
+            Vocabulary = text.Split("\r\n").ToList();
+        }
+
+        Button lastClickedButton = null!;
+        int clickCount = 1;
+
+        #region Button Check
+
+        public void ZeroOrSpace(TextBlock textBlock)
+        {
+            if (clickCount > 2) clickCount = 1;
+
+            switch (clickCount)
+            {
+                case 1:
+                    inputBuffer.Append(textBlock.Text);
+                    break;
+                case 2:
+                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                    inputBuffer.Append(" ");
+                    break;
+            }
+        }
+
+        public string SevenOrNine(TextBlock textBlock)
+        {
+            if (clickCount > 5) clickCount = 1;
+            switch (clickCount)
+            {
+                case 1:
+                    return textBlock.Text;
+                case 2:
+                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                    return t9Mappings[textBlock.Text[0]][0].ToString();
+                case 3:
+                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                    return t9Mappings[textBlock.Text[0]][1].ToString();
+                case 4:
+                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                    return t9Mappings[textBlock.Text[0]][2].ToString();
+                case 5:
+                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                    return t9Mappings[textBlock.Text[0]][3].ToString();
+            }
+            return null!;
+        }
+
+        public string TwoToEight(TextBlock textBlock)
+        {
+            if (clickCount > 4) clickCount = 1;
+            switch (clickCount)
+            {
+                case 1:
+                    return textBlock.Text;
+                case 2:
+                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                    return t9Mappings[textBlock.Text[0]][0].ToString();
+                case 3:
+                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                    return t9Mappings[textBlock.Text[0]][1].ToString();
+                case 4:
+                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                    return t9Mappings[textBlock.Text[0]][2].ToString();
+            }
+            return null!;
+        }
+
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            bool zeroClicked = false;
+            if (lastClickedButton == clickedButton) clickCount++;
+            else
+            {
+                lastClickedButton = clickedButton;
+                clickCount = 1;
+            }
+
+            string buttonText = clickedButton.Content.ToString()!;
+
+            if (clickedButton.Content is Grid grid)
+            {
+                if (grid.Children.Count > 0 && grid.Children[0] is TextBlock textBlock) ZeroOrSpace(textBlock);
+                zeroClicked = true;
+            }
+            else
+            {
+
+                if (clickedButton.Content is StackPanel stackPanel)
+                {
+                    if (stackPanel.Children.Count > 0 && stackPanel.Children[0] is TextBlock textBlock)
+                    {
+                        if (textBlock.Text != "9" && textBlock.Text != "7") buttonText = TwoToEight(textBlock);
+                        else buttonText = SevenOrNine(textBlock);
+                        inputBuffer.Append(buttonText);
+                    }
+                }
+                else inputBuffer.Append(buttonText);
+            }
+
+            UpdateInputDisplay();
+            if (!zeroClicked)
+                Task.Run(() => { RecommendWord(); });
+
+
+        }
+
+        private void UpdateInputDisplay() => txtInput.Text = inputBuffer.ToString();
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (inputBuffer.Length != 0)
+            {
+                lastClickedButton = null!;
+                clickCount = 1;
+                inputBuffer.Remove(inputBuffer.Length - 1, 1);
+                UpdateInputDisplay();
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            inputBuffer.Clear();
+            UpdateInputDisplay();
+        }
+
+        private void RecommendWord()
+        {
+            Dispatcher.Invoke(() =>
+            {
+
+                List<string> wordsWrited = txtInput.Text.Split(' ', '*', '#').ToList();
+                string recommended = Vocabulary.FirstOrDefault(v => v.StartsWith(wordsWrited.Last()))!;
+                if (recommended != null)
+                {
+
+                    int lastSpaceIndex = txtInput.Text.LastIndexOf(wordsWrited.Last());
+
+                    int len = txtInput.Text.Length;
+
+                    txtInput.Text = txtInput.Text[..lastSpaceIndex] + recommended;
+
+                    txtInput.Select(len, txtInput.Text.Length);
+
+                    txtInput.Focus();
+                }
+            });
+        }
+
+        private void txtInput_PreviewKeyDown(object sender, KeyEventArgs e) => e.Handled = true;
+
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
         }
 
         public void SynvoOfMascaras()
@@ -44,23 +222,6 @@ namespace T9_WPF
             eight.Content = $"   8\nTUV";
             nine.Content = $"   9\nWXYZ";
             zero.Content = $"0\n+";
-
-
-        }
-
-        private void one_Click(object sender, RoutedEventArgs e)
-        {
-
-                MessageBox.Show("salam");
- 
-        }
-        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.D1)
-            {
-                
-                MessageBox.Show("NumPad1 key pressed!");
-            }
         }
     }
 }
---
+++ T9_WPF/T9Converter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace T9_WPF
+{
+    public class T9Converter
+    {
+        private Dictionary<char, string> t9Dictionary;
+        private Dictionary<char, int> keyPressCount;
+        private char[] keyPressOrder;
+
+        public T9Converter()
+        {
+            InitializeT9Dictionary();
+            InitializeKeyPressOrder();
+        }
+
+        private void InitializeT9Dictionary()
+        {
+            // T9 harf kombinasyonlarını belirleyen bir sözlük oluşturuyoruz.
+            t9Dictionary = new Dictionary<char, string>
+            {
+                {'2', "abc"},
+                {'3', "def"},
+                {'4', "ghi"},
+                {'5', "jkl"},
+                {'6', "mno"},
+                {'7', "pqrs"},
+                {'8', "tuv"},
+                {'9', "wxyz"}
+            };
+        }
+
+        private void InitializeKeyPressOrder()
+        {
+            // Her tuşun kaç defa basılması durumunda sıradaki harfi alacağını belirleyen bir sözlük oluşturuyoruz.
+            keyPressCount = new Dictionary<char, int>
+            {
+                {'2', 1},
+                {'3', 1},
+                {'4', 1},
+                {'5', 1},
+                {'6', 1},
+                {'7', 1},
+                {'8', 1},
+                {'9', 1}
+            };
+
+            // Her tuşun üzerinde dolaşırken sıradaki harfini almak için kullanılacak dizi.
+            keyPressOrder = new char[] { ' ', ' ', ' ', ' ' };
+        }
+
+        public async Task<string> ConvertT9ToTextAsync(string inputNumbers)
+        {
+            return await Task.Run(() =>
+            {
+                string result = "";
+                foreach (char number in inputNumbers)
+                {
+                    if (t9Dictionary.TryGetValue(number, out string letters))
+                    {
+                        int pressCount = keyPressCount[number];
+
+                        // Tuşa basıldıkça sıradaki harfi alıyoruz ve sayacı güncelliyoruz.
+                        char currentLetter = t9Dictionary[number][pressCount - 1];
+                        keyPressCount[number] = (pressCount % letters.Length) + 1;
+
+                        // Önceki tuşa basıldıysa, önceki tuşun sıradaki harfini tekrar alıyoruz.
+                        if (number == keyPressOrder[pressCount])
+                            currentLetter = t9Dictionary[number][pressCount];
+
+                        result += currentLetter;
+                        keyPressOrder[pressCount] = number;
+                    }
+                    else
+                    {
+                        // Eğer sayı geçersizse aynı sayıyı sonuca ekliyoruz.
+                        result += number;
+                        keyPressCount[number] = 1;
+                        keyPressOrder[1] = ' ';
+                        keyPressOrder[2] = ' ';
+                        keyPressOrder[3] = ' ';
+                    }
+                }
+
+                return result;
+            });
+        }
+    }
+}
