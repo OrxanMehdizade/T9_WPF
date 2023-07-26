bcabb43edf582612ebb3bc525251d0dadf3312b0
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-07-26 15:21:34 +0400

    new guncel

--- T9_WPF/MainWindow.xaml.cs
+++ T9_WPF/MainWindow.xaml.cs
@@ -3,23 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace T9_WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private Dictionary<char, string> t9Mappings = new Dictionary<char, string>
@@ -34,177 +23,135 @@ namespace T9_WPF
             {'9', "wxyz"}
         };
 
-        private StringBuilder inputBuffer = new();
-        private List<string> Vocabulary = new();
+        private List<string> oneriler = new List<string>();
+        public List<string> vocabulary = new List<string>();
+ 
 
         public MainWindow()
         {
             InitializeComponent();
-            StartFillVocabulary();
+
+            // Düğme tıklama olaylarını bağlıyoruz
+            // ...
+
+            // TextBox için TextChanged olayını bağlıyoruz
+            txtInput.TextChanged += TxtInput_TextChanged;
+            LoadVocabulary("Vocabulary.txt");
             SynvoOfMascaras();
         }
 
-        private void StartFillVocabulary()
+        private void LoadVocabulary(string filePath)
         {
-            string text = File.ReadAllText(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Vocabulary.txt"));
-            Vocabulary = text.Split("\r\n").ToList();
-        }
-
-        Button lastClickedButton = null!;
-        int clickCount = 1;
-
-        #region Button Check
-
-        public void ZeroOrSpace(TextBlock textBlock)
-        {
-            if (clickCount > 2) clickCount = 1;
-
-            switch (clickCount)
+            try
             {
-                case 1:
-                    inputBuffer.Append(textBlock.Text);
-                    break;
-                case 2:
-                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                    inputBuffer.Append(" ");
-                    break;
+                // Vocabulary.txt dosyasından kelimeleri okuyup vocabulary listesine ekliyoruz
+                string[] lines = File.ReadAllLines(filePath);
+                vocabulary = lines.Select(line => line.Trim().ToLower()).ToList();
+                
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Vocabulary dosyası yüklenirken bir hata oluştu: " + ex.Message);
             }
         }
 
-        public string SevenOrNine(TextBlock textBlock)
+        private async void TxtInput_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (clickCount > 5) clickCount = 1;
-            switch (clickCount)
-            {
-                case 1:
-                    return textBlock.Text;
-                case 2:
-                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                    return t9Mappings[textBlock.Text[0]][0].ToString();
-                case 3:
-                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                    return t9Mappings[textBlock.Text[0]][1].ToString();
-                case 4:
-                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                    return t9Mappings[textBlock.Text[0]][2].ToString();
-                case 5:
-                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                    return t9Mappings[textBlock.Text[0]][3].ToString();
-            }
-            return null!;
+            await Task.Delay(100); // Asenkron davranışı simüle etmek için küçük bir gecikme ekliyoruz (TPL)
+
+            // Kullanıcının girdisiyle uygun kelime önerilerini buluyoruz
+            string girdiMetni = txtInput.Text.ToLower();
+            oneriler = vocabulary.Where(kelime => kelime.StartsWith(girdiMetni)).ToList();
+
+            // Önerileri kullanıcıya gösteriyoruz
+            GosterOneriler();
         }
 
-        public string TwoToEight(TextBlock textBlock)
+        private void GosterOneriler()
         {
-            if (clickCount > 4) clickCount = 1;
-            switch (clickCount)
+
+            suggestionsPanel.Children.Clear();
+            foreach (var oneri in oneriler)
             {
-                case 1:
-                    return textBlock.Text;
-                case 2:
-                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                    return t9Mappings[textBlock.Text[0]][0].ToString();
-                case 3:
-                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                    return t9Mappings[textBlock.Text[0]][1].ToString();
-                case 4:
-                    inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                    return t9Mappings[textBlock.Text[0]][2].ToString();
-            }
-            return null!;
-        }
-
-        #endregion
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Button clickedButton = (Button)sender;
-            bool zeroClicked = false;
-            if (lastClickedButton == clickedButton) clickCount++;
-            else
-            {
-                lastClickedButton = clickedButton;
-                clickCount = 1;
-            }
-
-            string buttonText = clickedButton.Content.ToString()!;
-
-            if (clickedButton.Content is Grid grid)
-            {
-                if (grid.Children.Count > 0 && grid.Children[0] is TextBlock textBlock) ZeroOrSpace(textBlock);
-                zeroClicked = true;
-            }
-            else
-            {
-
-                if (clickedButton.Content is StackPanel stackPanel)
+                var oneriDugme = new Button
                 {
-                    if (stackPanel.Children.Count > 0 && stackPanel.Children[0] is TextBlock textBlock)
+                    Content = oneri,
+                    Margin = new Thickness(5)
+                };
+
+                oneriDugme.Click += OneriDugme_Click;
+                suggestionsPanel.Children.Add(oneriDugme);
+            }
+        }
+
+        private void OneriDugme_Click(object sender, RoutedEventArgs e)
+        {
+            var dugme = (Button)sender;
+            string secilenKelime = dugme.Content.ToString();
+            txtInput.Text = secilenKelime;
+        }
+
+
+
+
+
+
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            // "Add" adındaki butona tıklandığında buradaki kod çalışır
+            // Bu örnekte "Add" butonuna tıklandığında TextBox'a girilen metni önerilere ekliyoruz.
+
+            if (!string.IsNullOrEmpty(txtInput.Text))
+            {
+                string yeniOneri = txtInput.Text.Trim();
+
+                // Yeni öneriyi zaten listede yoksa ve boş değilse ekleyelim
+                if (!string.IsNullOrEmpty(yeniOneri) && !oneriler.Contains(yeniOneri) && !vocabulary.Contains(yeniOneri.Trim().ToLower()))
+                {
+                    oneriler.Add(yeniOneri);
+
+                    // Yeni öneriyi düğme olarak ekleyelim
+                    var oneriDugme = new Button
                     {
-                        if (textBlock.Text != "9" && textBlock.Text != "7") buttonText = TwoToEight(textBlock);
-                        else buttonText = SevenOrNine(textBlock);
-                        inputBuffer.Append(buttonText);
+                        Content = yeniOneri,
+                        Margin = new Thickness(5)
+                    };
+                    oneriDugme.Click += OneriDugme_Click;
+                    suggestionsPanel.Children.Add(oneriDugme);
+
+                    vocabulary.Add(yeniOneri);
+
+                    // Güncellenmiş vocabulary listesini dosyaya geri yazıyoruz
+                    try
+                    {
+                        File.AppendAllText("Vocabulary.txt", yeniOneri + Environment.NewLine);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Vocabulary dosyasına yazılırken bir hata oluştu: " + ex.Message);
                     }
                 }
-                else inputBuffer.Append(buttonText);
             }
 
-            UpdateInputDisplay();
-            if (!zeroClicked)
-                Task.Run(() => { RecommendWord(); });
-
-
+            // TextBox'a girilen metni temizleyelim
+            txtInput.Clear();
         }
 
-        private void UpdateInputDisplay() => txtInput.Text = inputBuffer.ToString();
 
-        private void DeleteButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (inputBuffer.Length != 0)
-            {
-                lastClickedButton = null!;
-                clickCount = 1;
-                inputBuffer.Remove(inputBuffer.Length - 1, 1);
-                UpdateInputDisplay();
-            }
-        }
 
-        private void ClearButton_Click(object sender, RoutedEventArgs e)
-        {
-            inputBuffer.Clear();
-            UpdateInputDisplay();
-        }
 
-        private void RecommendWord()
-        {
-            Dispatcher.Invoke(() =>
-            {
 
-                List<string> wordsWrited = txtInput.Text.Split(' ', '*', '#').ToList();
-                string recommended = Vocabulary.FirstOrDefault(v => v.StartsWith(wordsWrited.Last()))!;
-                if (recommended != null)
-                {
 
-                    int lastSpaceIndex = txtInput.Text.LastIndexOf(wordsWrited.Last());
-
-                    int len = txtInput.Text.Length;
-
-                    txtInput.Text = txtInput.Text[..lastSpaceIndex] + recommended;
-
-                    txtInput.Select(len, txtInput.Text.Length);
-
-                    txtInput.Focus();
-                }
-            });
-        }
-
-        private void txtInput_PreviewKeyDown(object sender, KeyEventArgs e) => e.Handled = true;
 
 
         private void C_Click(object sender, RoutedEventArgs e)
         {
             txtInput.Clear();
         }
+
+
 
         public void SynvoOfMascaras()
         {
