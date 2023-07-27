0c955972171ba94b6cc08b15d1c053a7e70a9185
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-07-27 11:54:02 +0400

    button eror

--- T9_WPF/MainWindow.xaml.cs
+++ T9_WPF/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace T9_WPF
 {
@@ -23,18 +24,12 @@ namespace T9_WPF
             {'9', "wxyz"}
         };
 
-        private List<string> oneriler = new List<string>();
+        private List<string> recommendations = new List<string>();
         public List<string> vocabulary = new List<string>();
- 
 
         public MainWindow()
         {
             InitializeComponent();
-
-            // Düğme tıklama olaylarını bağlıyoruz
-            // ...
-
-            // TextBox için TextChanged olayını bağlıyoruz
             txtInput.TextChanged += TxtInput_TextChanged;
             LoadVocabulary("Vocabulary.txt");
             SynvoOfMascaras();
@@ -44,105 +39,83 @@ namespace T9_WPF
         {
             try
             {
-                // Vocabulary.txt dosyasından kelimeleri okuyup vocabulary listesine ekliyoruz
                 string[] lines = File.ReadAllLines(filePath);
                 vocabulary = lines.Select(line => line.Trim().ToLower()).ToList();
-                
-                
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Vocabulary dosyası yüklenirken bir hata oluştu: " + ex.Message);
+                MessageBox.Show("Vocabulary faylını yükləyərkən səhv oldu: " + ex.Message);
             }
         }
 
         private async void TxtInput_TextChanged(object sender, TextChangedEventArgs e)
         {
-            await Task.Delay(100); // Asenkron davranışı simüle etmek için küçük bir gecikme ekliyoruz (TPL)
+            await Task.Delay(100); // Asenkron davranışdı gözlədirik kiçik müddətli
 
-            // Kullanıcının girdisiyle uygun kelime önerilerini buluyoruz
-            string girdiMetni = txtInput.Text.ToLower();
-            oneriler = vocabulary.Where(kelime => kelime.StartsWith(girdiMetni)).ToList();
+            // daxil etmə ilə eyni olanları göstərən LINQ
+            string includedtxt = txtInput.Text.ToLower();
+            recommendations = vocabulary.Where(kelime => kelime.StartsWith(includedtxt)).ToList();
 
-            // Önerileri kullanıcıya gösteriyoruz
-            GosterOneriler();
+            // göstərmək üçün olan metod 
+            ShowingSuggestions();
         }
 
-        private void GosterOneriler()
+        private void ShowingSuggestions()
         {
-
             suggestionsPanel.Children.Clear();
-            foreach (var oneri in oneriler)
+            foreach (var item in recommendations)
             {
-                var oneriDugme = new Button
+                var recommendationbtn = new Button
                 {
-                    Content = oneri,
+                    Content = item,
                     Margin = new Thickness(5)
                 };
 
-                oneriDugme.Click += OneriDugme_Click;
-                suggestionsPanel.Children.Add(oneriDugme);
+                recommendationbtn.Click += suggestionbtn_Click;
+                suggestionsPanel.Children.Add(recommendationbtn);
             }
         }
 
-        private void OneriDugme_Click(object sender, RoutedEventArgs e)
+        private void suggestionbtn_Click(object sender, RoutedEventArgs e)
         {
-            var dugme = (Button)sender;
-            string secilenKelime = dugme.Content.ToString();
-            txtInput.Text = secilenKelime;
+            var btn = (Button)sender;
+            string selectabletxt = btn.Content.ToString();
+            txtInput.Text = selectabletxt;
         }
-
-
-
-
-
-
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            // "Add" adındaki butona tıklandığında buradaki kod çalışır
-            // Bu örnekte "Add" butonuna tıklandığında TextBox'a girilen metni önerilere ekliyoruz.
-
             if (!string.IsNullOrEmpty(txtInput.Text))
             {
-                string yeniOneri = txtInput.Text.Trim();
+                string newtxt = txtInput.Text.Trim();
 
-                // Yeni öneriyi zaten listede yoksa ve boş değilse ekleyelim
-                if (!string.IsNullOrEmpty(yeniOneri) && !oneriler.Contains(yeniOneri) && !vocabulary.Contains(yeniOneri.Trim().ToLower()))
+                if (!string.IsNullOrEmpty(newtxt) && !recommendations.Contains(newtxt) && !vocabulary.Contains(newtxt.Trim().ToLower()))
                 {
-                    oneriler.Add(yeniOneri);
+                    recommendations.Add(newtxt);
 
-                    // Yeni öneriyi düğme olarak ekleyelim
-                    var oneriDugme = new Button
+                    var suggestionbtn = new Button
                     {
-                        Content = yeniOneri,
+                        Content = newtxt,
                         Margin = new Thickness(5)
                     };
-                    oneriDugme.Click += OneriDugme_Click;
-                    suggestionsPanel.Children.Add(oneriDugme);
+                    suggestionbtn.Click += suggestionbtn_Click;
+                    suggestionsPanel.Children.Add(suggestionbtn);
 
-                    vocabulary.Add(yeniOneri);
+                    vocabulary.Add(newtxt);
 
-                    // Güncellenmiş vocabulary listesini dosyaya geri yazıyoruz
                     try
                     {
-                        File.AppendAllText("Vocabulary.txt", yeniOneri + Environment.NewLine);
+                        File.AppendAllText("Vocabulary.txt", newtxt + Environment.NewLine);
                     }
                     catch (Exception ex)
                     {
-                        MessageBox.Show("Vocabulary dosyasına yazılırken bir hata oluştu: " + ex.Message);
+                        MessageBox.Show("Vocabulary faylına yazarkən səhv baş verdi:" + ex.Message);
                     }
                 }
             }
 
-            // TextBox'a girilen metni temizleyelim
             txtInput.Clear();
         }
-
-
-
-
-
 
 
 
@@ -150,8 +123,6 @@ namespace T9_WPF
         {
             txtInput.Clear();
         }
-
-
 
         public void SynvoOfMascaras()
         {
